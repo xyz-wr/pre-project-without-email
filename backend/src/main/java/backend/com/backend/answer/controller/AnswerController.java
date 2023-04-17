4a6335271afd49bf8b3722c22ae13d81c6ca63a3
NOTE111 <lolly5488@gmail.com>
2023-04-17 15:44:43 +0900

    Test : for test

--- backend/src/main/java/backend/com/backend/answer/controller/AnswerController.java
+++ backend/src/main/java/backend/com/backend/answer/controller/AnswerController.java
@@ -1,6 +1,7 @@
 package backend.com.backend.answer.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AnswerController {
     @PostMapping
     public ResponseEntity postAnswer() {
+        return null;
+    }
+    @GetMapping("/")
+    public ResponseEntity getAnswers() {
         return null;
     }
 }
