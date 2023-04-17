26475be57b4332ca750b04fbdada1bf5889c22d5
NOTE111 <lolly5488@gmail.com>
2023-04-17 15:04:33 +0900

    Feat : Auditable 및 AuditorAwareImpl 추가했습니다. 각자의 Entity에다가 상속하면 생성일, 수정일, 작성자 등의 컬럼정보가 자동으로 채워집니다. usage ex) public class Question extends Auditable {}

---
+++ backend/src/main/java/backend/com/backend/answer/controller/AnswerController.java
@@ -0,0 +1,9 @@
+package backend.com.backend.answer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class AnswerController {
+}
