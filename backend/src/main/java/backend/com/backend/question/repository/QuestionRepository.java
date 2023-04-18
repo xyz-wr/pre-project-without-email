2b66e2cf3298deee001545316b4321a5fd29b471
xyz-wr <pponi13468@naver.com>
2023-04-18 19:48:38 +0900

    question crud 구현

---
+++ backend/src/main/java/backend/com/backend/question/repository/QuestionRepository.java
@@ -0,0 +1,10 @@
+package backend.com.backend.question.repository;
+
+import backend.com.backend.question.entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+    Optional<Question> findById(Long questionId);
+}
