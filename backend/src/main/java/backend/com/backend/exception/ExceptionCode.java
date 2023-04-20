d2cca794c794637e9869050982eb613c7d958c4b
Dongyeop Lee <dlduq29@gmail.com>
2023-04-21 01:54:40 +0900

    test2

--- backend/src/main/java/backend/com/backend/exception/ExceptionCode.java
+++ backend/src/main/java/backend/com/backend/exception/ExceptionCode.java
@@ -6,7 +6,8 @@ public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "Member Not Found"),// 존재하지 않는 회원
     QUESTION_NOT_FOUND(404,"Question Not Found"),//존재하지 않는 질문
     ANSWER_NOT_FOUND(404, "Answer Not Found"), //존재하지 않는 답변
-    MEMBER_EXISTS(409, "Member exists");
+    MEMBER_EXISTS(409, "Member exists"),
+    QUESTION_EXISTS(409,"Question NOT FOUND");
     @Getter
     private final int status;
 
